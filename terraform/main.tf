85a0b0e4636be24678104febbfa1a3ead4c3de5d
Athul S <athuls121@gmail.com>
2023-11-04 16:10:07 -0400

    terraform commit V1

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  credentials = file("path/to/your/credentials.json")
+  project     = "your-project-id"
+  region      = "us-central1"  # Replace with your desired region
+}
+
+module "gke" {
+  source     = "./gke"
+  project_id = var.project_id
+}
+
+module "redis" {
+  source     = "./redis"
+  project_id = var.project_id
+}
