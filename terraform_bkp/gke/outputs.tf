53c8cced409b4917cbcba09522c68747cb669e98
Athul S <athuls121@gmail.com>
2023-11-29 03:51:50 -0500

    test 7

--- terraform_bkp/gke/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_name" {
-  value = google_container_cluster.my_cluster.name
-}
-
-output "cluster_endpoint" {
-  value = google_container_cluster.my_cluster.endpoint
-}
--- terraform_bkp/redis/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "project_id" {
-  description = "The ID of the Google Cloud project"
-}
