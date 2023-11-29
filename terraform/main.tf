1b74c98f7e02b231d1f8a21d0ec4e8fb312e2be9
Athul S <athuls121@gmail.com>
2023-11-29 17:22:40 -0500

    Deployment Testing - 11

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,12 @@ data "google_redis_instance" "existing_redis_instance" {
   name = "redis"
 }
 
-# Conditionally create Redis instance if it doesn't exist
 resource "google_redis_instance" "example" {
   count               = length(data.google_redis_instance.existing_redis_instance) > 0 ? 0 : 1
   name                = "redis"
-  tier                = "BASIC"
-  memory_size_gb      = 1
-  authorized_network  = "projects/cellular-way-399223/global/networks/default"
+  tier                = "BASIC"  
+  memory_size_gb      = 1             
+  authorized_network  = "projects/cellular-way-399223/global/networks/default"  
 }
 
 # Check if GKE cluster exists
@@ -24,7 +23,6 @@ data "google_container_cluster" "existing_gke_cluster" {
   location = "us-central1"
 }
 
-# Conditionally create GKE cluster if it doesn't exist
 resource "google_container_cluster" "primary" {
   count               = length(data.google_container_cluster.existing_gke_cluster) > 0 ? 0 : 1
   name                = "gcp-devops-project-clust"
